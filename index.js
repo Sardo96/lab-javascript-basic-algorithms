5a71208bf63ca8e55f51ea0f7087b15260cfbba4
Miguel Barroca Sardo <miguel.sardo@constru.pt>
2023-02-18 17:31:38 +0000

    Done

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 // Iteration 1: Names and Input
-
+const hacker1 = 'Miguel';
+console.log(`The driver's name is ${hacker1}.`);
+const hacker2 = 'Diogo';
+console.log(`The navigator's name is ${hacker2}.`);
 
 // Iteration 2: Conditionals
-
+if (hacker1.length > hacker2.length) {
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters.`
+  );
+} else if (hacker1.length < hacker2.length) {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
+  );
+} else {
+  console.log(
+    `Wow, you both have equally long names, ${hacker1.length} characters!`
+  );
+}
 
 // Iteration 3: Loops
+
+const driversUpCase = hacker1.toUpperCase();
+console.log(driversUpCase.split('').join(' '));
+
+let navigatorReverse = '';
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  navigatorReverse += hacker2[i];
+}
+console.log(navigatorReverse);
+
+let result = hacker1.localeCompare(hacker2);
+if (result < 0) {
+  console.log(`The driver's name goes first.`);
+} else if (result > 0) {
+  console.log(`Yo, the navigator goes first definitely.`);
+} else {
+  console.log(`What?! You both have the same name?`);
+}
